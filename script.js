0f071647fbaf5907c980a965f7c8e3d851e27eea
said-ops <idrissisaid29@gmail.com>
2024-08-06 21:33:29 +0000

    add cart item

--- script.js
+++ script.js
@@ -51,6 +51,120 @@ function populateItems (items) {
         itemsContainer.appendChild(div);                  
     });
 }
-fetchData();
+//add to cart
+function addToCart (event){
+    const addbutton = event.target;
+    const cart = document.querySelector('.cart');
+    const cartEmpty = document.querySelector('.cart-empty');
+    if(addbutton.className === 'add-text'){
+        console.log('add clicked');
+        console.log(event.target.parentElement.querySelector('.increment'));
+        //display cart and hide cart empty
+        if(cart.style.display === 'none' || cart.style.display === ''){
+            displayCart();
+            displayEncrement(event.target.parentElement.querySelector('.increment'));
+            hideAddToCart(event.target.parentElement.querySelector('img'),event.target.parentElement.querySelector('span.add-text'));
+            addCartItem(event.target.parentElement.parentElement,cart);
+            plusMinus(event.target.parentElement.querySelector('.increment'),event.target.parentElement.parentElement,cart);
+            console.log(event.target.parentElement.parentElement);
+        }else
+        //if cart is already displayed
+        if(cart.style.display === 'flex'){
+            //hide this add cart and display encrement
+            displayEncrement(event.target.parentElement.querySelector('.increment'));
+            hideAddToCart(event.target.parentElement.querySelector('img'),event.target.parentElement.querySelector('span.add-text'));
+            addCartItem(event.target.parentElement.parentElement,cart);
+            plusMinus(event.target.parentElement.querySelector('.increment'),event.target.parentElement.parentElement,cart);
+            console.log(event.target.parentElement.parentElement);
+        }
+    }
+}
+//display the cart
+function displayCart(){
+    const cart = document.querySelector('.cart');
+    const cartEmpty = document.querySelector('.cart-empty');
+    cart.style.display = 'flex';
+    cartEmpty.style.display = 'none';
+    console.log('cart-opend');
+}
+//display encrement
+function displayEncrement(encrement){
+    encrement.style.display = 'flex';
+}
+//hide add to card
+function hideAddToCart(img,text){
+    img.style.display = 'none';
+    text.style.display = 'none';
+}
+//increment and decrement item order number
+function plusMinus(increment,item,cart){
+    let counter = 1;
+    increment.querySelector('.max').addEventListener('click', ()=>{
+        console.log('clicked+');
+        counter++;
+    increment.querySelector('.counter').textContent = counter;
+    //increment the order number on cart 
+    cart.querySelector('.cart-items').querySelectorAll('.cart-item').forEach(cartItem => {
+        if(item.querySelector('.item-infos .name').textContent === cartItem.querySelector('.cart-item-title').textContent){
+            cartItem.querySelector('.cart-item-infos .item-number').textContent = counter + 'x';
+        }
+    });
+
+    });
+    increment.querySelector('.min').addEventListener('click', ()=>{
+        if(counter === 1){
+            increment.querySelector('.counter').textContent = '1';
+            
+        }else{
+            counter--;
+            increment.querySelector('.counter').textContent = counter;
+            console.log('clicked-');
+            //decrement the order number on cart 
+            cart.querySelector('.cart-items').querySelectorAll('.cart-item').forEach( cartItem =>{
+        if(item.querySelector('.item-infos .name').textContent === cartItem.querySelector('.cart-item-title').textContent){
+            cartItem.querySelector('.cart-item-infos .item-number').textContent = counter + 'x';
+        }
+    });
+        }
+    
+    });
+}
+//add the item infos to the cart
+function addCartItem(item,cart){
+    //get the item details
+    const itemTitle = item.querySelector('.item-infos .name').textContent;
+    const orderNumber = item.querySelector('.add-to-cart .increment .counter').textContent;
+    const itemPrice = item.querySelector(' .item-infos .item-price .price').textContent;
+    //item view
+    const cartItemView = `<span class="cart-item-title">${itemTitle}</span>
+    <span class="remove-item"><img src="./assets/images/icon-remove-item.svg" alt="remove item"></span>
+    <div class="cart-item-infos">
+      <span class="item-number">${orderNumber}x</span>
+      <span class="unit-price">
+        <span>@</span>
+        <span>${itemPrice}</span>
+      </span>
+      <span class="sub-total">$${parseFloat(itemPrice.substr(1,itemPrice.lenght) * orderNumber).toFixed(2)}</span>
+    </div>`;
+    const div =  document.createElement('div');
+    div.classList.add('cart-item');
+    div.innerHTML = cartItemView;
+    cart.querySelector('.cart-items').appendChild(div);
+}
+//get order number
+function getOrderNumber(counter){
+    return counter;
+}
+//attach eventListeners
+function attacheEL (){
+    //items
+    const items = document.querySelector('.items');
+    items.addEventListener('click', addToCart);
+}
+//dom event
+document.addEventListener('DOMContentLoaded', function (e){
+    fetchData();
+    attacheEL();
+});
 
 
